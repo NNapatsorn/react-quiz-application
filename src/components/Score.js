e9c671dba8cc3d4ce928107f4f998a2a339f50b7
NNapatsorn <napatsorn.nilas@gmail.com>
2022-09-26 17:01:48 +0700

    Done

--- src/components/Score.js
+++ src/components/Score.js
@@ -1,11 +1,24 @@
-const Score = () => {
-    return (
-        <>
-            <div className="score">
-                <h1>Score Component</h1>
-            </div>
-        </>
-    )
-}
+import { useContext } from "react";
+import { DataContext } from "../App";
+import QuestionsData from "./data/QuestionsData";
 
-export default Score
+const Score = () => {
+  const { score, setAppState, setScore } = useContext(DataContext);
+  const restartApp = () => {
+    setScore(0);
+    setAppState("menu");
+  };
+  return (
+    <>
+      <div className='score'>
+        <h1>Total Scores</h1>
+        <h2>
+          {score}/ {QuestionsData.length}
+        </h2>
+        <button onClick={restartApp}>Play Again</button>
+      </div>
+    </>
+  );
+};
+
+export default Score;
